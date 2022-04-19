2c96985de0bc13470d4c1a75110898a2df2ed9fa
Yufan Sheng <yufan@streamnative.io>
2022-04-19 11:28:16 +0800

    feat: add back the telegram API ratelimit.

--- telegram/executor.go
+++ telegram/executor.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"os"
 	"path"
+	"time"
 
 	"github.com/gotd/contrib/middleware/floodwait"
+	"github.com/gotd/contrib/middleware/ratelimit"
 	"github.com/gotd/td/session"
 	"github.com/gotd/td/telegram"
 	"github.com/gotd/td/telegram/dcs"
 	"golang.org/x/net/proxy"
+	"golang.org/x/time/rate"
 
 	"github.com/bibliolater/bookhunter/pkg/log"
 )
@@ -47,6 +50,7 @@ func (e *executor) Execute(f func(context.Context, *telegram.Client) error) erro
 			SessionStorage: &session.FileStorage{Path: e.sessionPath},
 			Middlewares: []telegram.Middleware{
 				floodwait.NewSimpleWaiter().WithMaxRetries(uint(e.config.Retry)),
+				ratelimit.New(rate.Every(100*time.Millisecond), 5),
 			},
 		},
 	)
