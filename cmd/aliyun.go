027dfd254141c31731a8b2c779e7a4a8c00dfce9
Yufan Sheng <yufan@streamnative.io>
2022-11-15 21:11:15 +0800

    feat: add refresh token acquire for aliyun driver.

---
+++ cmd/aliyun.go
@@ -0,0 +1,64 @@
+package cmd
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+
+	"github.com/chyroc/go-aliyundrive"
+	"github.com/spf13/cobra"
+
+	"github.com/bookstairs/bookhunter/cmd/flags"
+	"github.com/bookstairs/bookhunter/internal/client"
+	"github.com/bookstairs/bookhunter/internal/log"
+)
+
+var aliyunCmd = &cobra.Command{
+	Use:   "aliyun",
+	Short: "A command line tool for acquiring the refresh token from aliyundrive with QR code login.",
+	Run: func(cmd *cobra.Command, args []string) {
+		path := ""
+		if flags.ConfigRoot == "" {
+			root, err := client.DefaultConfigRoot()
+			if err != nil {
+				log.Fatal(err)
+			}
+			path = filepath.Join(root, "api.aliyundrive.com")
+		} else {
+			path = filepath.Join(flags.ConfigRoot, "api.aliyundrive.com")
+		}
+
+		// Create config directory.
+		err := os.MkdirAll(path, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Create the session file if it's not existed.
+		file := filepath.Join(path, "session.json")
+		open, err := os.OpenFile(file, os.O_RDONLY|os.O_CREATE, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_ = open.Close()
+
+		store := aliyundrive.NewFileStore(file)
+		ins := aliyundrive.New(aliyundrive.WithStore(store))
+		ctx := context.Background()
+
+		// Valid the token, we will sign in with QR code if this token is expired.
+		_, err = ins.Auth.LoginByQrcode(ctx, &aliyundrive.LoginByQrcodeReq{})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Access the token from the storage.
+		token, err := store.Get(ctx, "")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Info("Successfully sign into aliyun drive.")
+		log.Infof("Refresh Token: %s", token.RefreshToken)
+	},
+}
