a7a0e4ea5bb6dd08500f9465c651940f60b5c2ca
Yufan Sheng <yufan@streamnative.io>
2022-11-15 12:26:48 +0800

    feat: add lanzou driver.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/bookstairs/bookhunter
 go 1.19
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0
 	github.com/bits-and-blooms/bitset v1.3.3
 	github.com/go-resty/resty/v2 v2.7.0
 	github.com/jedib0t/go-pretty/v6 v6.4.2
@@ -10,19 +11,23 @@ require (
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db
 	github.com/schollz/progressbar/v3 v3.12.1
 	github.com/spf13/cobra v1.6.1
+	github.com/stretchr/testify v1.8.1
 	github.com/yi-ge/unzip v1.0.2
 	go.uber.org/ratelimit v0.2.0
 )
 
 require (
 	github.com/andres-erbsen/clock v0.0.0-20160526145045-9e14626cd129 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.2 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/testify v1.8.1 // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 	golang.org/x/term v0.2.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/driver/lanzou/lanzou_test.go
@@ -0,0 +1,82 @@
+package lanzou
+
+import (
+	"testing"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseLanzouUrl(t *testing.T) {
+	type args struct {
+		url string
+		pwd string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "test lanzoue.com",
+			args: args{
+				url: "https://tianlangbooks.lanzoue.com/iI0lc0fj3mpa",
+				pwd: "tlsw",
+			},
+		},
+		{
+			name: "test lanzoui",
+			args: args{
+				url: "https://tianlangbooks.lanzoui.com/i9CTIws9s6d",
+				pwd: "tlsw",
+			},
+		},
+		{
+			name: "test lanzouf.com",
+			args: args{
+				url: "https://tianlangbooks.lanzouf.com/ic7HY05ejl2h",
+				pwd: "tlsw",
+			},
+		},
+		{
+			name: "test lanzoup.com",
+			args: args{
+				url: "https://tianlangbooks.lanzoup.com/i4q4Chcm2cf",
+				pwd: "tlsw",
+			},
+		},
+		{
+			name: "test lanzouy.com",
+			args: args{
+				url: "https://fast8.lanzouy.com/ibZCg0b8tibi",
+				pwd: "",
+			},
+		},
+		{
+			name: "test sobook lanzouy.com",
+			args: args{
+				url: "https://sobooks.lanzoum.com/b03phl3te",
+				pwd: "htuj",
+			},
+		},
+		{
+			name: "test sobook lanzouy.com 2",
+			args: args{
+				url: "https://sobooks.lanzoum.com/ihOex0fiodri",
+				pwd: "",
+			},
+		},
+	}
+	client := NewDefaultDrive(resty.New())
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			response, err := client.ResolveShareURL(tt.args.url, tt.args.pwd)
+			assert.NoError(t, err, "Failed to resolve link")
+			assert.Equal(t, int64(200), response.Code, "Failed to resolve link: "+response.Msg)
+			assert.NotEmpty(t, response.Data)
+			assert.NotNil(t, response.Data)
+			assert.NotNil(t, response.Data.URL)
+			assert.NotNil(t, response.Data.Name)
+		})
+	}
+}
