92730370fd71f7fd5202f201e55c3dad2757ceaa
Yufan Sheng <yufan@streamnative.io>
2022-11-22 03:47:44 +0800

    fix: invalid build version.

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# We will add upx support for macOS 13, after the https://github.com/upx/upx/issues/612 issue get fixed.
+
 if [[ $1 == *"linux"* ]]; then
   upx --lzma "$1"
 elif [[ $1 == *"window"* ]]; then
