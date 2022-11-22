e15e234697e6f1c881872fec42a88338c76d0949
Yufan Sheng <yufan@streamnative.io>
2022-11-22 10:23:40 +0800

    build: remove upx compression.

--- scripts/compress.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# We will add upx support for macOS 13, after the https://github.com/upx/upx/issues/612 issue get fixed.
-
-if [[ $1 == *"linux"* ]]; then
-  upx --lzma "$1"
-elif [[ $1 == *"window"* ]]; then
-  upx -9 "$1"
-fi
